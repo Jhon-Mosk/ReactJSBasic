75722c5e9c33755d461ea9ad3a125e2d67b7e31b
Jhon Mosk <jhon-mosk@yandex.ru>
2021-08-07 23:06:02 +0300

    add middleware for bot answer, change bot name to chat name, add auto scroll page to down whe right message and open chat

--- app/src/components/MessageForm/messageForm.jsx
+++ app/src/components/MessageForm/messageForm.jsx
@@ -7,6 +7,7 @@ import generateBotPhrase from '../BotPhrase/botPhrase';
 import { checkKeyOnEnter } from '../../utils/checkKeyOnEnter';
 import { getInputVisibility } from '../../store/messageForm/selectors';
 import { getProfileName } from '../../store/profile/selectors';
+import { getChatList } from '../../store/chats';
 
 const useStyles = makeStyles(() => ({
     messageForm: {
@@ -30,6 +31,7 @@ const useStyles = makeStyles(() => ({
 function MessageForm() {
     const classes = useStyles();
     const profileName = useSelector(getProfileName);
+    const chatList = useSelector(getChatList);
     const inputVisibility = useSelector(getInputVisibility);
     const [value, setValue] = useState('');
     const { chatId } = useParams();
@@ -46,31 +48,35 @@ function MessageForm() {
     const handleChange = (event) => {
         setValue(event.target.value);
     }
-    //отправляем сообщение в хранилище
-    const sendUserMessage = useCallback(() => {
-        let author = profileName || 'Я';
-        dispatch(createAddMessage(chatId, author, value));
-        setValue('');
-        botAnswer();
-    }, [dispatch, chatId, value, profileName]);
 
-    //ответ бота на каждое сообщение
-    const botAnswer = useCallback(() => {
-        let botMessage = {
-            author: 'Бот',
-            text: generateBotPhrase(),
+    const getBotName = () => {
+        let botData = chatList.filter((item) => item.id === chatId);
+        return botData[0].name
+    }
+
+    //ответ бота на каждое сообщение WithThunk
+    const sendUserMessageWithThunk = (chatId, author, message) => (dispatch, getState) => {
+        dispatch(createAddMessage(chatId, author, message));
+        if (author !== 'Бот') {
+            let botMessage = {
+                author: getBotName(),
+                text: generateBotPhrase(),
+            };
+            setTimeout(() => dispatch(createAddMessage(chatId, botMessage.author, botMessage.text)), 1500);
         }
+    };
 
-        setTimeout(() => {
-            dispatch(createAddMessage(chatId, botMessage.author, botMessage.text));;
-        }, 1500);
-
-    }, [dispatch, chatId]);
+    //отправляем сообщение в хранилище WithThunk
+    const sendUserMessage = useCallback((message) => {
+        let author = profileName || 'Я';
+        dispatch(sendUserMessageWithThunk(chatId, author, message));
+        setValue('');
+    }, [chatId, dispatch, profileName]);
 
     //проверяем нажат ли энтер в поле ввода, если нажат отправляем сообщение
     const checkKey = (event) => {
         if (checkKeyOnEnter(event.code)) {
-            sendUserMessage();
+            sendUserMessage(value);
         };
     }
 
@@ -83,7 +89,7 @@ function MessageForm() {
     return (
         <div className={checkInputVisibility()}>
             <input ref={inputRef} className={classes.messageFormInput} type="text" value={value} onChange={handleChange} onKeyDown={checkKey} />
-            <button className={classes.messageFormButton} type="button" onClick={sendUserMessage}>Отправить</button>
+            <button className={classes.messageFormButton} type="button" onClick={() => sendUserMessage(value)}>Отправить</button>
         </div>
     )
 }
--- app/src/components/RenderCurrentMessages/renderCurrentMessages.jsx
+++ app/src/components/RenderCurrentMessages/renderCurrentMessages.jsx
@@ -2,10 +2,21 @@ import { useSelector } from 'react-redux';
 import { useParams } from "react-router-dom";
 import { shallowEqual } from "react-redux";
 import { getMessageList } from '../../store/messages/selectors';
+import { useEffect } from 'react';
 
 export default function RenderCurrentMessages() {
     const messageList = useSelector(getMessageList, shallowEqual);
     const { chatId } = useParams();
+
+    useEffect (() => {
+        let scrollHeight = Math.max(
+            document.body.scrollHeight, document.documentElement.scrollHeight,
+            document.body.offsetHeight, document.documentElement.offsetHeight,
+            document.body.clientHeight, document.documentElement.clientHeight
+        );
+        console.log(scrollHeight);
+        window.scrollBy(0,scrollHeight)
+    }, [chatId, messageList])
 
     function isEmpty(obj) {
         for (let key in obj) {
