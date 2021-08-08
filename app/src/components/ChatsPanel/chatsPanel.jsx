5bf984516812a5dae33bfa23281cbb0435d0d04f
Jhon Mosk <jhon-mosk@yandex.ru>
2021-08-08 21:26:16 +0300

    ?

--- app/src/components/ChatsPanel/chatsPanel.jsx
+++ app/src/components/ChatsPanel/chatsPanel.jsx
@@ -1,4 +1,6 @@
 import { useCallback, useState } from 'react';
+import { useDispatch } from 'react-redux';
+
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
@@ -11,14 +13,12 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-
-import RenderCurrentMessages from '../RenderCurrentMessages/renderCurrentMessages'
-// import MessageForm from '../MessageForm/messageForm'
-import Navigation from '../Navigation/navigation';
-import ChatListContainer from '../../containers/ChatListContainer';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
-import { useDispatch } from 'react-redux';
+
+import RenderCurrentMessages from '../RenderCurrentMessages/renderCurrentMessages'
+import Navigation from '../Navigation/navigation';
+import ChatListContainer from '../../containers/ChatListContainer';
 import { createAddChat } from '../../store/chats/actions';
 import MessageFormContainer from '../../containers/MessageFormContainer';
 
